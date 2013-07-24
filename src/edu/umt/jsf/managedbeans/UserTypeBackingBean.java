5cbe6727b5ebd08dd1fec3b1ffcdd40158e21ade
Erika Noble <erika.noble@mso.umt.edu>
2013-07-24 09:51:59 -0600

    Deleteusertype.xhtml deletes successfully

--- src/edu/umt/jsf/managedbeans/UserTypeBackingBean.java
+++ src/edu/umt/jsf/managedbeans/UserTypeBackingBean.java
@@ -7,35 +7,43 @@ public class UserTypeBackingBean {
 	private List<UserType> userTypes;
 	private UserType userType;
 	private String description;
-	
-	
+
 	public List<UserType> getUserTypes() {
 		return DatabaseManager.getUserTypes();
 	}
+
 	public void setUserTypes(List<UserType> userTypes) {
 		this.userTypes = userTypes;
 	}
+
 	public UserType getUserType() {
 		return userType;
 	}
+
 	public void setUserType(UserType userType) {
 		this.userType = userType;
 	}
+
 	public String getDescription() {
 		return description;
 	}
+
 	public void setDescription(String description) {
 		this.description = description;
 	}
-	
-	
-	public String newUserTypeAction() throws Exception{
+
+	public String newUserTypeAction() throws Exception {
 		UserType ut = new UserType();
 		ut.setDescription(this.getDescription());
-		
-			DatabaseManager.insertUsertype(ut);
-		
+
+		DatabaseManager.insertUsertype(ut);
+
 		return "ok";
 	}
-	
+
+	public String deleteUserTypeAction(UserType ut) throws Exception {
+		DatabaseManager.deleteUserType(ut);
+		return null;
+
+	}
 }
